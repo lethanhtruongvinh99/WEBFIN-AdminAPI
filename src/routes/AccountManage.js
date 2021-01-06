a1d2335d6b0a80ab1f5e1e9e6d5457c12950009d
Phan Nhật Vinh <phannhatvinh10a@gmail.com>
2021-01-06 23:19:19 +0700

    Added missing status response

--- src/routes/AccountManage.js
+++ src/routes/AccountManage.js
@@ -10,101 +10,136 @@ const {
 
 const router = express.Router();
 
-router.get("/", async (req, res) => {
+router.get("/", async (req, res) =>
+{
   //get All User
-  passport.authorize("jwt", { session: false }, async (err, user, info) => {
-    if (err) {
+  passport.authorize("jwt", { session: false }, async (err, user, info) =>
+  {
+    if (err)
+    {
       return res.status(400).json({ message: err.message });
     }
-    if (info) {
+    if (info)
+    {
       console.log(info);
       return res.status(400).json({ message: info.message });
-    } else {
+    } else
+    {
       const listUser = await getListUser();
-      if (listUser.status) {
-        res.json({ accounts: listUser.accounts });
-      } else {
-        res.json({ message: listUser });
+      if (listUser.status)
+      {
+        res.status(200).json({ accounts: listUser.accounts });
+      } else
+      {
+        res.status(200).json({ message: listUser });
       }
     }
   })(req, res);
 });
 
-router.post("/deactivate", async (req, res) => {
+router.post("/deactivate", async (req, res) =>
+{
   //body is accountId.
-  passport.authorize("jwt", { session: false }, async (err, user, info) => {
-    if (err) {
+  passport.authorize("jwt", { session: false }, async (err, user, info) =>
+  {
+    if (err)
+    {
       return res.status(400).json({ message: err.message });
     }
-    if (info) {
+    if (info)
+    {
       console.log(info);
       return res.status(400).json({ message: info.message });
-    } else {
+    } else
+    {
       const targetAccountId = req.body.accountId;
       const result = await deactivateAccount(targetAccountId);
-      if (result.status) {
+      if (result.status)
+      {
         return res.json({ auth: true, message: "Successfully!" });
-      } else {
+      } else
+      {
         return res.json({ auth: false, message: "Error" });
       }
     }
   })(req, res);
 });
 
-router.post("/activate", async (req, res) => {
-  passport.authorize("jwt", { session: false }, async (err, user, info) => {
-    if (err) {
+router.post("/activate", async (req, res) =>
+{
+  passport.authorize("jwt", { session: false }, async (err, user, info) =>
+  {
+    if (err)
+    {
       return res.status(400).json({ message: err.message });
     }
-    if (info) {
+    if (info)
+    {
       console.log(info);
       return res.status(400).json({ message: info.message });
-    } else {
+    } else
+    {
       const targetAccountId = req.body.accountId;
       const result = await activateAccount(targetAccountId);
-      if (result.status) {
+      if (result.status)
+      {
         return res.json({ auth: true, message: "Successfully!" });
-      } else {
+      } else
+      {
         return res.json({ auth: false, message: "Error" });
       }
     }
   })(req, res);
 });
 
-router.post("/profile", async (req, res) => {
-  passport.authorize("jwt", { session: false }, async (err, user, info) => {
-    if (err) {
+router.post("/profile", async (req, res) =>
+{
+  passport.authorize("jwt", { session: false }, async (err, user, info) =>
+  {
+    if (err)
+    {
       return res.status(400).json({ message: err.message });
     }
-    if (info) {
+    if (info)
+    {
       console.log(info);
       return res.status(400).json({ message: info.message });
-    } else {
+    } else
+    {
       const accountId = req.body.accountId;
       const accountProfile = await getUserById(accountId);
-      if (accountProfile.status) {
+      if (accountProfile.status)
+      {
         return res.json({ auth: true, account: accountProfile.account });
-      } else {
+      } else
+      {
         return res.json({ auth: false, message: accountProfile });
       }
     }
   })(req, res);
 });
 
-router.post("/find", async (req, res) => {
-  passport.authorize("jwt", { session: false }, async (err, user, info) => {
-    if (err) {
+router.post("/find", async (req, res) =>
+{
+  passport.authorize("jwt", { session: false }, async (err, user, info) =>
+  {
+    if (err)
+    {
       return res.status(400).json({ message: err.message });
     }
-    if (info) {
+    if (info)
+    {
       console.log(info);
       return res.status(400).json({ message: info.message });
-    } else {
+    } else
+    {
       const keyword = req.body.keyword;
       const result = await findAccount(keyword);
-      if (result.status) {
+      if (result.status)
+      {
         return res.json({ auth: true, account: result.result });
-      } else {
+      } else
+      {
         return res.json({ auth: false, message: result });
       }
     }
